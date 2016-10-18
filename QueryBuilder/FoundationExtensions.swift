e4580cb710f2a9b2593a498275b550713362fea4
rex-remind101 <rex@remind101.com>
2016-10-18 12:12:38 -0700

    [master] cleanup project, make query builder its own target

---
+++ QueryBuilder/FoundationExtensions.swift
@@ -0,0 +1,9 @@
+//
+//  FoundationExtensions.swift
+//  AutoGraph
+//
+//  Created by Rex Fenley on 10/18/16.
+//  Copyright © 2016 Remind. All rights reserved.
+//
+
+import Foundation
---
+++ QueryBuilder/QueryBuilder.h
@@ -0,0 +1,19 @@
+//
+//  QueryBuilder.h
+//  QueryBuilder
+//
+//  Created by Rex Fenley on 10/18/16.
+//  Copyright © 2016 Remind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QueryBuilder.
+FOUNDATION_EXPORT double QueryBuilderVersionNumber;
+
+//! Project version string for QueryBuilder.
+FOUNDATION_EXPORT const unsigned char QueryBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueryBuilder/PublicHeader.h>
+
+
