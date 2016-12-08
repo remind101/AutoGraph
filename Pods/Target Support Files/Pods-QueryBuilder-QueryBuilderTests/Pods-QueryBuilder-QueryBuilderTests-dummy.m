e940b83abe1607ab1cd9b3ed25957712d59bf559
rex-remind101 <rex@remind101.com>
2016-12-08 13:46:11 -0800

    [alamofire-pod] added alamofire

---
+++ Pods/Target Support Files/Pods-QueryBuilder-QueryBuilderTests/Pods-QueryBuilder-QueryBuilderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_QueryBuilder_QueryBuilderTests : NSObject
+@end
+@implementation PodsDummy_Pods_QueryBuilder_QueryBuilderTests
+@end
---
+++ Pods/Target Support Files/Pods-QueryBuilder-QueryBuilderTests/Pods-QueryBuilder-QueryBuilderTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_QueryBuilder_QueryBuilderTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_QueryBuilder_QueryBuilderTestsVersionString[];
+
