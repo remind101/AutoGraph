c1154639b5f98f5e09bd63d567b6213a5c5869fd
rex-remind101 <rex@remind101.com>
2017-01-04 12:32:11 -0800

    [errors] added error handling

--- AutoGraph/AutoGraph.swift
+++ AutoGraph/AutoGraph.swift
@@ -4,14 +4,14 @@ import Crust
 import JSONValueRX
 import QueryBuilder
 
-protocol Request {
+public protocol Request {
     associatedtype Mapping: Crust.Mapping
     
     var query: QueryBuilder.Operation { get }
     var mapping: Mapping { get }
 }
 
-typealias RequestCompletion<T: Request> = (_ object: T.Mapping.MappedObject?, _ error: Error?) -> ()
+typealias RequestCompletion<T: Request> = (_ result: Result<T.Mapping.MappedObject>) -> ()
 
 class AutoGraph {
     static var url = "http://localhost:8080/graphql"
@@ -22,12 +22,22 @@ class AutoGraph {
             do {
                 let value = response.result.value!
                 let json = try JSONValue(object: value)
-                let mapper = CRMapper<T.Mapping.MappedObject, T.Mapping>()
-                let result = try mapper.mapFromJSONToNewObject(json, mapping: request.mapping)
-                completion(result, nil)
+                
+                if let queryError = AutoGraphError(graphQLJSON: json) {
+                    throw queryError
+                }
+                
+                do {
+                    let mapper = CRMapper<T.Mapping.MappedObject, T.Mapping>()
+                    let result = try mapper.mapFromJSONToNewObject(json, mapping: request.mapping)
+                    completion(.success(result))
+                }
+                catch let e {
+                    throw AutoGraphError.mapping(error: e)
+                }
             }
             catch let e {
-                completion(nil, e)
+                completion(.failure(e))
             }
         }
     }
---
+++ AutoGraph/Errors.swift
@@ -0,0 +1,115 @@
+import Foundation
+import JSONValueRX
+
+/*
+ GraphQL errors have the following form:
+ 
+ {
+    "errors": [
+     {
+        "message": "Cannot query field \"d\" on type \"Planet\".",
+        "locations": [
+         {
+            "line": 18,
+            "column": 7
+         }
+        ]
+     },
+     {
+        "message": "Fragment \"planet\" is never used.",
+        "locations": [
+         {
+            "line": 23,
+            "column": 1
+         }
+        ]
+     }
+    ]
+ }
+ 
+*/
+
+enum AutoGraphError: LocalizedError {
+    case graphQL(errors: [GraphQLError])
+    case network(error: Error)
+    case mapping(error: Error)
+    case invalidQuery
+    
+    init?(graphQLJSON: JSONValue) {
+        guard let errorsJSON = graphQLJSON["errors"] else {
+            return nil
+        }
+        
+        guard case .array(let errors) = errorsJSON else {
+            self = .invalidQuery
+            return
+        }
+        
+        self = .graphQL(errors: errors.flatMap { GraphQLError(json: $0) })
+        return
+    }
+    
+    public var errorDescription: String? {
+        switch self {
+        case .graphQL(let errors):
+            return errors.flatMap { $0.localizedDescription }.joined(separator: "\n")
+            
+        case .network(let error):
+            return "Network Failure: " + error.localizedDescription
+            
+        case .mapping(let error):
+            return "Mapping Failure: " + error.localizedDescription
+        
+        case .invalidQuery:
+            return self.localizedDescription
+        }
+    }
+}
+
+public struct GraphQLError: LocalizedError {
+    
+    public struct Location: CustomStringConvertible {
+        public let line: Int
+        public let column: Int
+        
+        public var description: String {
+            return "line: \(line), column: \(column)"
+        }
+        
+        init?(json: JSONValue) {
+            guard case .some(.number(let line)) = json["line"] else {
+                return nil
+            }
+            
+            guard case .some(.number(let column)) = json["column"] else {
+                return nil
+            }
+            
+            self.line = Int(line)
+            self.column = Int(column)
+        }
+    }
+    
+    public let message: String
+    public let locations: [Location]
+    
+    public var errorDescription: String? {
+        return "GraphQL error message: \(self.message), locations: \(self.locations)"
+    }
+    
+    init(json: JSONValue) {
+        self.message = {
+            guard case .some(.string(let message)) = json["message"] else {
+                return ""
+            }
+            return message
+        }()
+        
+        self.locations = {
+            guard case .some(.array(let locations)) = json["locations"] else {
+                return []
+            }
+            return locations.flatMap { Location(json: $0) }
+        }()
+    }
+}
