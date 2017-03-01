9e71253c9741113d7ac1ad0e7368474371011bd6
rex-remind101 <rex@remind101.com>
2017-03-01 14:58:23 -0800

    [lifecycle] cleanup

--- AutoGraph/AutoGraph.swift
+++ AutoGraph/AutoGraph.swift
@@ -1,7 +1,4 @@
-import Alamofire
-import Crust
 import Foundation
-import JSONValueRX
 
 public protocol Cancellable {
     func cancelAll()
@@ -24,109 +21,6 @@ public protocol Client: RequestSender, Cancellable {
 public protocol ThreadUnsafe: class {
     static var primaryKeys: [String] { get }
     func value(forKeyPath keyPath: String) -> Any?
-}
-
-extension Int: AnyMappable { }
-class VoidMapping: AnyMapping {
-    typealias AdaptorKind = AnyAdaptorImp<MappedObject>
-    typealias MappedObject = Int
-    func mapping(tomap: inout Int, context: MappingContext) { }
-}
-
-// TODO: We should support non-equatable collections.
-// TOOD: We should better apply currying and futures to clean some of this up.
-public enum ResultBinding<M: Mapping, CM: Mapping, C: RangeReplaceableCollection>
-where C.Iterator.Element == CM.MappedObject, CM.MappedObject: Equatable {
-    
-    case object(mappingBinding: () -> Binding<M>, completion: RequestCompletion<M.MappedObject>)
-    case collection(mappingBinding: () -> Binding<CM>, completion: RequestCompletion<C>)
-}
-
-public protocol LifeCycleRequest {
-    associatedtype Result
-}
-
-open class LifeCycle<R: LifeCycleRequest> {
-    open func willSend(request: R) throws { }
-    open func didFinish(result: Result<R.Result>) throws { }
-}
-
-open class GlobalLifeCycle {
-    open func willSend<R: Request>(request: R) throws { }
-    open func didFinish<R: Request>(result: Result<R.Result>) throws { }
-}
-
-public protocol Request {
-    /// The `Mapping` used to map from the returned JSON payload to a concrete type
-    /// `Mapping.MappedObject`.
-    associatedtype Mapping: Crust.Mapping
-    
-    /// The returned type for the request.
-    /// E.g if the requests returns an array then change to `[Mapping.MappedObject]`.
-    associatedtype Result = Mapping.MappedObject
-    
-    associatedtype Query: GraphQLQuery
-    
-    /// Hooks for the life cycle of the request.
-    func willSend() throws
-    func didFinish(result: AutoGraphQL.Result<Result>) throws
-    
-    /// The query to be sent to GraphQL.
-    var query: Query { get }
-    
-    /// The mapping to use when mapping JSON into the a concrete type.
-    ///
-    /// **WARNING:**
-    ///
-    /// `mapping` does NOT execute on the main thread. It's important that any `Adaptor`
-    /// used by `mapping` establishes it's own connection to the DB from within `mapping`.
-    ///
-    /// Additionally, the mapped data (`Mapping.MappedObject`) is assumed to be safe to pass
-    /// across threads unless it inherits from `ThreadUnsafe`. 
-    var mapping: Binding<Mapping> { get }
-}
-
-extension Request {
-    func willSend() throws { }
-    func didFinish(result: AutoGraphQL.Result<Result>) throws { }
-}
-
-extension Request
-    where Result: RangeReplaceableCollection,
-    Result.Iterator.Element == Mapping.MappedObject,
-    Mapping.MappedObject: Equatable {
-    
-    func generateBinding(completion: @escaping RequestCompletion<Result>) -> ResultBinding<Mapping, Mapping, Result> {
-        let didFinish = self.didFinish
-        let lifeCycleCompletion: RequestCompletion<Result> = { result in
-            do {
-                try didFinish(result)
-                completion(result)
-            }
-            catch let e {
-                completion(.failure(e))
-            }
-        }
-        
-        return ResultBinding<Mapping, Mapping, Result>.collection(mappingBinding: { self.mapping }, completion: lifeCycleCompletion)
-    }
-}
-
-extension Request where Result == Mapping.MappedObject {
-    func generateBinding(completion: @escaping RequestCompletion<Mapping.MappedObject>) -> ResultBinding<Mapping, VoidMapping, Array<Int>> {
-        let didFinish = self.didFinish
-        let lifeCycleCompletion: RequestCompletion<Result> = { result in
-            do {
-                try didFinish(result)
-                completion(result)
-            }
-            catch let e {
-                completion(.failure(e))
-            }
-        }
-        
-        return ResultBinding<Mapping, VoidMapping, Array<Int>>.object(mappingBinding: { self.mapping }, completion: lifeCycleCompletion)
-    }
 }
 
 public typealias RequestCompletion<R> = (_ result: Result<R>) -> ()
---
+++ AutoGraph/Request.swift
@@ -0,0 +1,93 @@
+import Crust
+import Foundation
+
+extension Int: AnyMappable { }
+class VoidMapping: AnyMapping {
+    typealias AdaptorKind = AnyAdaptorImp<MappedObject>
+    typealias MappedObject = Int
+    func mapping(tomap: inout Int, context: MappingContext) { }
+}
+
+// TODO: We should support non-equatable collections.
+// TOOD: We should better apply currying and futures to clean some of this up.
+public enum ResultBinding<M: Mapping, CM: Mapping, C: RangeReplaceableCollection>
+where C.Iterator.Element == CM.MappedObject, CM.MappedObject: Equatable {
+    
+    case object(mappingBinding: () -> Binding<M>, completion: RequestCompletion<M.MappedObject>)
+    case collection(mappingBinding: () -> Binding<CM>, completion: RequestCompletion<C>)
+}
+
+public protocol Request {
+    /// The `Mapping` used to map from the returned JSON payload to a concrete type
+    /// `Mapping.MappedObject`.
+    associatedtype Mapping: Crust.Mapping
+    
+    /// The returned type for the request.
+    /// E.g if the requests returns an array then change to `[Mapping.MappedObject]`.
+    associatedtype Result = Mapping.MappedObject
+    
+    associatedtype Query: GraphQLQuery
+    
+    /// The query to be sent to GraphQL.
+    var query: Query { get }
+    
+    /// The mapping to use when mapping JSON into the a concrete type.
+    ///
+    /// **WARNING:**
+    ///
+    /// `mapping` does NOT execute on the main thread. It's important that any `Adaptor`
+    /// used by `mapping` establishes it's own connection to the DB from within `mapping`.
+    ///
+    /// Additionally, the mapped data (`Mapping.MappedObject`) is assumed to be safe to pass
+    /// across threads unless it inherits from `ThreadUnsafe`.
+    var mapping: Binding<Mapping> { get }
+    
+    /// Called at the moment before the request will be sent from the Client.
+    func willSend() throws
+    
+    /// Called right before calling the completion handler for the sent request.
+    func didFinish(result: AutoGraphQL.Result<Result>) throws
+}
+
+extension Request {
+    func willSend() throws { }
+    func didFinish(result: AutoGraphQL.Result<Result>) throws { }
+}
+
+extension Request
+    where Result: RangeReplaceableCollection,
+    Result.Iterator.Element == Mapping.MappedObject,
+Mapping.MappedObject: Equatable {
+    
+    func generateBinding(completion: @escaping RequestCompletion<Result>) -> ResultBinding<Mapping, Mapping, Result> {
+        let didFinish = self.didFinish
+        let lifeCycleCompletion: RequestCompletion<Result> = { result in
+            do {
+                try didFinish(result)
+                completion(result)
+            }
+            catch let e {
+                completion(.failure(e))
+            }
+        }
+        
+        return ResultBinding<Mapping, Mapping, Result>.collection(mappingBinding: { self.mapping }, completion: lifeCycleCompletion)
+    }
+}
+
+extension Request where Result == Mapping.MappedObject {
+    func generateBinding(completion: @escaping RequestCompletion<Mapping.MappedObject>) -> ResultBinding<Mapping, VoidMapping, Array<Int>> {
+        let didFinish = self.didFinish
+        let lifeCycleCompletion: RequestCompletion<Result> = { result in
+            do {
+                try didFinish(result)
+                completion(result)
+            }
+            catch let e {
+                completion(.failure(e))
+            }
+        }
+        
+        return ResultBinding<Mapping, VoidMapping, Array<Int>>.object(mappingBinding: { self.mapping }, completion: lifeCycleCompletion)
+    }
+}
