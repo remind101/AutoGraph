f21773b0bf7afddab95f2beb2cc5f01a438dd5ff
rex-remind101 <rex@remind101.com>
2017-02-28 17:02:30 -0800

    [new-crust] make it compile

--- AutoGraphTests/Realm/RealmMappings.swift
+++ AutoGraphTests/Realm/RealmMappings.swift
@@ -89,25 +89,23 @@ public class RealmAdaptor: Adaptor {
         }
     }
     
-    public func fetchObjects(type: RLMObject.Type, primaryKeyValues: [[String : CVarArg]], isMapping: Bool) -> ResultsType? {
+    public func sanitize(primaryKeyProperty property: String, forValue value: CVarArg, ofType type: RLMObject.Type) -> CVarArg? {
         
         // Since Date is converted as such so often we won't require implementors to write their own transform.
-        
-        func sanitize(key: String, value: NSObject) -> NSObject {
-            if type.isProperty(key, ofType: NSDate.self), case let value as String = value {
-                return Date(isoString: value)! as NSDate
-            }
-            return type.sanitizeValue(value, fromProperty: key, realm: self.realm)
+        if type.isProperty(property, ofType: NSDate.self), case let value as String = value {
+            return Date(isoString: value)! as NSDate
         }
+        return type.sanitizeValue(value, fromProperty: property, realm: self.realm)
+    }
+    
+    public func fetchObjects(type: RLMObject.Type, primaryKeyValues: [[String : CVarArg]], isMapping: Bool) -> ResultsType? {
         
         var totalPredicate = [NSPredicate]()
         
         for keyValues in primaryKeyValues {
             var objectPredicates = [NSPredicate]()
             for (key, var value) in keyValues {
-                if case let obj as NSObject = value {
-                    value = sanitize(key: key, value: obj)
-                }
+                value = self.sanitize(primaryKeyProperty: key, forValue: value, ofType: type) ?? value
                 let predicate = NSPredicate(format: "%K == %@", key, value)
                 objectPredicates.append(predicate)
             }
@@ -128,7 +126,7 @@ public class RealmAdaptor: Adaptor {
         .filter {
             predicate.evaluate(with: $0)
         }
-
+        
         if objects.count > 0 {
             return Array(objects)
         }
@@ -153,9 +151,9 @@ extension RLMArray {
         }
         return index
     }
-
+    
     public typealias Index = UInt
-
+    
     public func append(_ newElement: RLMObject) {
         self.addObjectNonGeneric(newElement)
     }
@@ -214,6 +212,10 @@ public class RealmSwiftObjectAdaptorBridge<T>: Adaptor {
         try self.realmObjCAdaptor.deleteObject(rlmObj)
     }
     
+    public func sanitize(primaryKeyProperty property: String, forValue value: CVarArg, ofType type: BaseType.Type) -> CVarArg? {
+        return self.realmObjCAdaptor.sanitize(primaryKeyProperty: property, forValue: value, ofType: type as! RLMObject.Type)
+    }
+    
     public func fetchObjects(type: BaseType.Type, primaryKeyValues: [[String : CVarArg]], isMapping: Bool) -> ResultsType? {
         guard let rlmObjects = self.realmObjCAdaptor.fetchObjects(type: self.rlmObjectType,
                                                                   primaryKeyValues: primaryKeyValues,
@@ -229,7 +231,6 @@ public class RealmSwiftObjectAdaptorBridge<T>: Adaptor {
 /// Wrapper used to map `RLMObjects`. Relies on `RLMArrayBridge` since `RLMArray` does not support `RangeReplaceableCollection`.
 public class RLMArrayMappingBridge<T: RLMObject>: Mapping {
     public typealias MappedObject = T
-    public typealias SequenceKind = RLMArrayBridge<MappedObject>
     
     public let adaptor: RealmSwiftObjectAdaptorBridge<MappedObject>
     public let primaryKeys: [Mapping.PrimaryKeyDescriptor]?
@@ -252,7 +253,7 @@ public class RLMArrayMappingBridge<T: RLMObject>: Mapping {
     }
 }
 
-public extension Binding where M: RealmMapping, M.MappedObject: RLMObject, M.SequenceKind.Iterator.Element == M.MappedObject {
+public extension Binding where M: RealmMapping, M.MappedObject: RLMObject {
     
     func generateRLMArrayMappingBridge() -> Binding<RLMArrayMappingBridge<M.MappedObject>> {
         
@@ -263,47 +264,19 @@ public extension Binding where M: RealmMapping, M.MappedObject: RLMObject, M.Seq
             
         case .collectionMapping(let keyPath, let mapping, let updatePolicy):
             let bridge = RLMArrayMappingBridge<M.MappedObject>(rlmObjectMapping: mapping)
-            
-            let bridgedInsert: CollectionInsertionMethod<RLMArrayBridge<M.MappedObject>> = {
-                switch updatePolicy.insert {
-                    
-                case .append:
-                    return .append
-                    
-                case .replace(let deletion):
-                    guard let deletion = deletion else {
-                        return .replace(delete: nil)
-                    }
-                    
-                    let bridgedDeletion = { (objs: RLMArrayBridge<M.MappedObject>) -> RLMArrayBridge<M.MappedObject> in
-                        let rlmObjsToDelete = deletion( objs.map { $0 } as! M.SequenceKind )
-                        let listToDelete = RLMArrayBridge<M.MappedObject>()
-                        for obj in rlmObjsToDelete {
-                            listToDelete.append(obj)
-                        }
-                        return listToDelete
-                    }
-                    return .replace(delete: bridgedDeletion)
-                }
-            }()
-            
-            return .collectionMapping(keyPath, bridge, (insert: bridgedInsert, unique: updatePolicy.unique))
+            return .collectionMapping(keyPath, bridge, updatePolicy)
         }
     }
 }
 
-// TODO: Currently this leads to "Ambiguous use of operator '<-'" conflicting with the `RangeReplaceableCollection` version.
-// Considering this is more specialized and that `RLMArray` does not implement `RangeReplaceableCollection` this seems like
-// a bug. Report the bug. In the meantime we're forced to break convention and use `map(toRLMArray:using:)` instead of `<-`.
-
 @discardableResult
-public func <- <T: RLMObject, U: RealmMapping, C: MappingContext>(field: RLMArray<T>, binding:(key: Binding<U>, context: C)) -> C where U.MappedObject == T, U.SequenceKind.Iterator.Element == U.MappedObject, T: Equatable {
+public func <- <T: RLMObject, U: RealmMapping, C: MappingContext>(field: RLMArray<T>, binding:(key: Binding<U>, context: C)) -> C where U.MappedObject == T, T: Equatable {
     
     return map(toRLMArray: field, using: binding)
 }
 
 @discardableResult
-public func map<U: RealmMapping, C: MappingContext>(toRLMArray field: RLMArray<U.MappedObject>, using binding:(key: Binding<U>, context: C)) -> C where U.SequenceKind.Iterator.Element == U.MappedObject, U.MappedObject: Equatable {
+public func map<T: RLMObject, U: RealmMapping, C: MappingContext>(toRLMArray field: RLMArray<T>, using binding:(key: Binding<U>, context: C)) -> C where U.MappedObject == T, T: Equatable {
     
     var variableList = RLMArrayBridge(rlmArray: field)
     let bridge = binding.key.generateRLMArrayMappingBridge()
