91691efb65f4eab6673e3f1ae099b94e26c7b16e
rex-remind101 <rex@remind101.com>
2017-04-06 17:34:43 -0700

    [void-thread-adapter] added unsafe thread adapter that allows for pure pass-through of objects

--- AutoGraphTests/FilmRequest.swift
+++ AutoGraphTests/FilmRequest.swift
@@ -90,3 +90,59 @@ class FilmStub: Stub {
         set { }
     }
 }
+
+class FilmThreadUnconfinedRequest: ThreadUnconfinedRequest {
+    /*
+     query film {
+        film(id: "ZmlsbXM6MQ==") {
+            title
+            episodeID
+            director
+            openingCrawl
+        }
+     }
+     */
+    
+    typealias SerializedObject = FilmThreadUnconfined
+    
+    let query = Operation(type: .query,
+                          name: "film",
+                          fields: [
+                            Object(name: "film",
+                                   alias: nil,
+                                   fields: [
+                                    "id",
+                                    Scalar(name: "title", alias: nil),
+                                    Scalar(name: "episodeID", alias: nil),
+                                    Scalar(name: "director", alias: nil),
+                                    Scalar(name: "openingCrawl", alias: nil)],
+                                   arguments: ["id" : "ZmlsbXM6MQ=="])
+                        ])
+    
+    var mapping: Binding<FilmThreadUnconfinedMapping> {
+        return Binding.mapping("data.film", FilmThreadUnconfinedMapping())
+    }
+    
+    public func willSend() throws { }
+    public func didFinishRequest(response: HTTPURLResponse?, json: JSONValue) throws { }
+    public func didFinish(result: Result<Film>) throws { }
+}
+
+class FilmThreadUnconfinedMapping: AnyMapping {
+    typealias AdapterKind = AnyAdapterImp<FilmThreadUnconfined>
+    
+    public func mapping(toMap: inout FilmThreadUnconfined, context: MappingContext) {
+        toMap.title         <- ("title", context)
+        toMap.episode       <- ("episodeID", context)
+        toMap.openingCrawl  <- ("openingCrawl", context)
+        toMap.director      <- ("director", context)
+    }
+}
+
+struct FilmThreadUnconfined: AnyMappable {
+    var remoteId: String?
+    var title: String?
+    var episode: Int?
+    var openingCrawl: String?
+    var director: String?
+}
