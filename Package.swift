d8200c83415bf386ee29ed2e8ae2f6085495a49b
rex remind <rex@remind101.com>
2020-10-02 15:17:11 -0700

    [websocket_subscriptions] package manager changes

--- Package.swift
+++ Package.swift
@@ -18,11 +18,16 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMinor(from: "5.0.5")),
         .package(url: "https://github.com/rexmas/JSONValue.git", .upToNextMinor(from: "7.0.0")),
+        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMinor(from: "4.0.0")),
     ],
     targets: [
         .target(
             name: "AutoGraphQL",
-            dependencies: ["Alamofire", .product(name: "JSONValueRX", package: "JSONValue")],
+            dependencies: [
+                "Alamofire",
+                .product(name: "JSONValueRX", package: "JSONValue"),
+                "Starscream",
+            ],
             path: ".",
             exclude: ["AutoGraph/Info.plist", "QueryBuilder/Info.plist"],
             sources: ["AutoGraph", "QueryBuilder"]
